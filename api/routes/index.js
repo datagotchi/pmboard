a34e14e87350d57015c4d89d36c7088151bcfa0e
Bob Stark <bob.stark@collusionapp.com>
2015-03-18 20:00:41 -0700

    Added node backend (2)

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'EcoBoard' });
 });
 
+router.put('/usertypes', function(req, res, next) {
+	res.send('Got a PUT request at /usertypes');
+});
+
 module.exports = router;
