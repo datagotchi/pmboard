abdc84bcc8ac6e6e4426cb164bb47a118ce10fde
Bob Stark <bob.stark@collusionapp.com>
2015-03-18 20:28:18 -0700

    Fixed express app to use routing correctly & removed extraneous node/express packages

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EcoBoard' });
+  res.send('Welcome to EcoBoard');
 });
 
 router.put('/usertypes', function(req, res, next) {
-	res.send('Got a PUT request at /usertypes');
+	res.status(400) // invalid request
+		.send('Got a PUT request at /usertypes');
 });
 
 module.exports = router;
