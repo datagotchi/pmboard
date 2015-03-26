74ab52769c878eb153ecdf5a9ccb1a7c79477c35
Bob Stark <bob.stark@collusionapp.com>
2015-03-25 21:28:04 -0700

    Fixed widget framework with personas adding/editing/deleting as a test case

--- js/main.js
+++ js/main.js
@@ -54,21 +54,8 @@ function userResearchInfo() {
 //$(document).ready(function() {
 function init() {
   
-  $(window).load(function() {
-      $('#widgets').boardwidget({
-        title: 'Test Header',
-        columns: ['Col1'],
-        rows: ['testval'],
-        addmoreText: "Add another Test",
-        addmoreAtts: {
-          id: 'addtest'
-        }
-      });
-    });
-  /*
-  
   product_url = "http://localhost:3000/products/" + prod_id;
-  personas_url = product_url + "/personas";
+  
   for (var p = 0; p < products.length; p++) {
     if (products[p].id == prod_id) {
       $("#products").append('<li class="active"><a href="#">' + products[p].name + '</a></li>');
@@ -87,87 +74,54 @@ function init() {
 	$('input:file').change(function() {
 		$('[data-target="#requestEvaluation"]').prop('disabled', false);
 	});
-	$(document).on('click', '.remove-persona', function(e) {
-		var This = this;
-		$.ajax({
-			method: 'delete',
-			url: personas_url,
-			dataType: 'json',
-			contentType: 'application/json; charset=utf-8',
-			data: JSON.stringify({
-  			pk: $(This).next().attr("data-pk")
-			}),
-			//'{"pk": "' + $(This).next().attr("data-pk") + '"}',
-			success: function() {
-				$(This).parent().remove();
-			}
-		});
-	});
+	
+	personas_url = product_url + "/personas";
+	$('#widgets').boardwidget({
+    title: 'Who are your users?',
+    columns: ['Evidence', 'Name'],
+    wrappers: [
+      '<button type="button" class="evidence btn btn-default label-danger label" data-toggle="popover" data-placement="top" data-trigger="focus" title="Number of pieces of evidence" data-content="lorem ipsum">',
+      '<a href="#" class="editable-value editable-click" data-name="persona{i}" data-type="text" data-pk="{i}">'
+    ],
+    api: personas_url,
+    addmoreText: "Add another user type",
+    addmoreAtts: {
+      id: 'newpersona'
+    },
+    deleteItem: function(Elem) {
+  		$.ajax({
+  			method: 'delete',
+  			url: personas_url, // this.options.api
+  			dataType: 'json',
+  			contentType: 'application/json; charset=utf-8',
+  			data: JSON.stringify({
+    			pk: $(Elem).parent().find('a.editable-value').attr("data-pk")
+  			}),
+  			success: function() {
+  				$(Elem).parent().remove();
+  			}
+  		});
+    }
+  });
 	
 	$.get(product_url, function (data) {
 		var prod_name = data.name;
 		$("#productName").text(prod_name);
 	});
-	
-	$('.new-value').editable({
-		value: '',
-		showbuttons: false,
-		params: function(params) { return JSON.stringify(params); },
-		onblur: 'submit',
-		url: personas_url,
-		ajaxOptions: {
-			type: 'put',
-			dataType: 'json',
-			contentType: 'application/json; charset=utf-8'
-		},
-		success: function(response, newValue) {
-			if (typeof response == "object") {
-				if (response.success) {
-					updatePersonas(response.personas);
-					$(this).editable('toggle');
-				} else {
-					return response.error;
-				}
-			} 
-		}
-	});
-	
-	function updatePersonas(personas) {
-		$("#personas .list-group").empty();
-		for (var i = 0; i < personas.length; i++) {
-			var persona = personas[i];
-			$("#personas .list-group").append(
-				'<li class="list-group-item input-group">'
-		    + '<button class="btn btn-default glyphicon glyphicon-remove remove-persona" type="button"></button>'
-    		+ '<a href="#" class="editable-value editable-click" data-name="persona' + i + '" data-type="text" data-pk="' + i +'">' + persona.name + '</a>&nbsp;&nbsp;' 
-    		+ '<button type="button" class="evidence btn btn-default label-danger label" data-toggle="popover" data-placement="top" data-trigger="focus" title="Number of pieces of evidence" data-content="lorem ipsum">0</button>'
-        + '</li>'
-			);
-		}
 		
-		$('.editable-value').editable({
-			showbuttons: false,
-			params: function(params) { return JSON.stringify(params); },
-			onblur: 'submit',
-			url: personas_url,
-			ajaxOptions: {
-				type: 'post',
-				dataType: 'json',
-				contentType: 'application/json; charset=utf-8'
-			},
-			success: function(response, newValue) {
-				if (typeof response == "object" && !response.success) {
-					return response.error;
-				}
-			},
-			error: function(a, b) {
-				console.err(a, b);
-			}
-		});
-	}
-		
-	$.get(personas_url, updatePersonas);
+	//$.get(personas_url, updatePersonas);
 	
 //});
-*/
+/* */
 }
+	/*$("#personas .list-group").empty();
+	for (var i = 0; i < personas.length; i++) {
+		var persona = personas[i];
+		$("#personas .list-group").append(
+			'<li class="list-group-item input-group">'
+	    + '<button class="btn btn-default glyphicon glyphicon-remove remove-persona" type="button"></button>'
+  		+ '<a href="#" class="editable-value editable-click" data-name="persona' + i + '" data-type="text" data-pk="' + i +'">' + persona.name + '</a>&nbsp;&nbsp;' 
+  		+ '<button type="button" class="evidence btn btn-default label-danger label" data-toggle="popover" data-placement="top" data-trigger="focus" title="Number of pieces of evidence" data-content="lorem ipsum">0</button>'
+      + '</li>'
+		);
+	}*/
--- js/widget.js
+++ js/widget.js
@@ -3,13 +3,19 @@ $.widget("pmboard.boardwidget", {
   // custom properties
   inputs: [],
   outputs: [],
+  rows: [
+    {name: "Type1", evidence: 5}, // test data
+    {name: "Type2", evidence: 0}
+  ],
   
+  // properties to specialize the widget (mostly specified on constructor, but can also be changed via _setOptions)
   options: {
     template: "widget.html",
     title: "",
     columns: [],
-    rows: [],
-    addmoreText: "Add",
+    wrappers: [], // e.g., <a>, <span>, etc.
+    api: "",
+    addmoreText: "Add", // TODO: use the footer for something else? 
     addmoreAtts: {},
     // intended result: <a href="#" class="editable-value new-value" id="newpersona" data-type="text" data-pk="new"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add another feature</a>
     
@@ -32,58 +38,118 @@ $.widget("pmboard.boardwidget", {
             .text(This.options.columns[i])
             .appendTo(tHead);
         }
+        if (This.options.deleteItem) { 
+          tHead.append('<th>Delete</th>');
+          $(document).on('click', '.remove-item', function(e) {
+        		var Elem = this;
+        		/*$.ajax({
+        			method: 'delete',
+        			url: This.options.api,
+        			dataType: 'json',
+        			contentType: 'application/json; charset=utf-8',
+        			data: JSON.stringify({
+          			pk: $(Elem).next().attr("data-pk")
+        			}),
+        			//'{"pk": "' + $(This).next().attr("data-pk") + '"}',
+        			success: function() {
+        				$(Elem).parent().remove();
+        			}
+        		});*/
+        		This.options.deleteItem(Elem);
+        	});
+        }
         tHead.appendTo(el.find('.panel-body .table'));
         
-        
+        var addmore = $('<a href="#" class="editable-value new-value" data-type="text" data-pk="new"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> ' + This.options.addmoreText + '</a>');
         addmoreAttsKeys = Object.keys(This.options.addmoreAtts);
-        if (addmoreAttsKeys.length > 0) {
-          var a = $('<a href="#" class="editable-value new-value" data-type="text" data-pk="new"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> ' + This.options.addmoreText + '</a>');
-          // TODO: extract an option 
-          for (var i = 0; i < addmoreAttsKeys.length; i++) {
-            var key = addmoreAttsKeys[i];
-            a.attr(key, This.options.addmoreAtts[key]);
-          }
-          el.find('.panel-footer').append(a);
+        // TODO: extract an option 
+        for (var i = 0; i < addmoreAttsKeys.length; i++) {
+          var key = addmoreAttsKeys[i];
+          addmore.attr(key, This.options.addmoreAtts[key]);
         }
+        addmore.editable({
+      		value: '',
+      		showbuttons: false,
+      		params: function(params) { return JSON.stringify(params); },
+      		onblur: 'submit',
+      		url: This.options.api,
+      		ajaxOptions: {
+      			type: 'put',
+      			dataType: 'json',
+      			contentType: 'application/json; charset=utf-8'
+      		},
+      		success: function(response, newValue) {
+      			if (typeof response == "object") {
+      				if (response.success) {
+      					This._refresh();
+      					$(this).editable('toggle');
+      				} else {
+      					return response.error;
+      				}
+      			} 
+      		}
+      	});
+        el.find('.panel-footer').append(addmore);
       el.appendTo(This.element);
+      
+      This._refresh();
     });
   },
   
   _refresh: function() {
     // fill in grid values
-    for (var i = 0; i < this.options.rows.length; i++) {
-      var row = this.options.rows[i];
+    if (this.options.api) {
+      var This = this;
+      $.get(this.options.api, function(personas) {
+        This.rows = personas;
+        This._refreshHelper();
+      });
+    } else {
+      this._refreshHelper();
+    }
+  },
+  
+  _refreshHelper: function() {
+    
+    var delCol = '';
+    if (this.options.deleteItem) {
+      delCol = '<button class="btn btn-default glyphicon glyphicon-remove remove-item" type="button"></button>';
+    }
+    
+    // refresh rows
+    var tbl = this.element.find('.table'); // FIXME: this will find .table in #widgets i think, not this single widget
+    tbl.find('tr').remove();
+    for (var i = 0; i < this.rows.length; i++) {
+      var row = this.rows[i];
       var tr = $('<tr>');
       for (var j = 0; j < this.options.columns.length; j++) {
+        var col = this.options.columns[j].toLowerCase();
+        var val = row[col] ? $(this.options.wrappers[j].replace(/{i}/g, i)).append(row[col]) : $(this.options.wrappers[j]).append(0);
         var td = $('<td>')
-          .append(row[j]);
+          .append(val)
+          .appendTo(tr);
       }
-      tr.appendTo(this.element.find('.table'));
+      if (delCol) $(delCol).appendTo(tr);
+      tr.appendTo(tbl); 
     }
+    $('.editable-value').editable({
+  		showbuttons: false,
+  		params: function(params) { return JSON.stringify(params); },
+  		onblur: 'submit',
+  		url: personas_url,
+  		ajaxOptions: {
+  			type: 'post',
+  			dataType: 'json',
+  			contentType: 'application/json; charset=utf-8'
+  		},
+  		success: function(response, newValue) {
+  			if (typeof response == "object" && !response.success) {
+  				return response.error;
+  			}
+  		},
+  		error: function(a, b) {
+  			console.err(a, b);
+  		}
+  	});
   }
 });
-
-// TODOs: 
-    
-    // render 'add more' button
-    // write to/render on page/return as value to be written on page
-  
-/* // old version
-  
-  // properties
-  this.title = "";
-  this.columns = [];
-  this.data = []; // to populate the table as rows
-  this.elemId = ""; 
-
-  // methods
-  this.render = function() {
-    // get widget.html template
-    // load into dom document
-    // fill in grid values via jquery selections
-    // render 'add more' button
-    // write to/render on page/return as value to be written on page
-  };
-  
-  // constructor code
-*/
